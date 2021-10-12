2ada6a7e645c8034e6af161d53a1044f1e9ed127
kazu-S-1110 <67362767+kazu-S-1110@users.noreply.github.com>
2021-10-13 06:16:51 +0900

    論理演算子を学習。基本すぎる。

---
+++ logical.go
@@ -0,0 +1,16 @@
+package main
+
+import "fmt"
+
+// 論理演算子
+
+func main() {
+	fmt.Println(true && false == true)  //false
+	fmt.Println(true && false == false) //true
+
+	fmt.Println(true || false)         //true
+	fmt.Println(true || false == true) //true
+
+	fmt.Println(!true)  //false
+	fmt.Println(!false) //true
+}
