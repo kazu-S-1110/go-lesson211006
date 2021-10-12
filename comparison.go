6d36f12543b0de764189715a9769c265c73cdf89
kazu-S-1110 <67362767+kazu-S-1110@users.noreply.github.com>
2021-10-13 06:11:13 +0900

    比較演算子について学習。もっと応用的な使い方はないのかな。

---
+++ comparison.go
@@ -0,0 +1,20 @@
+package main
+
+import "fmt"
+
+// 比較演算子
+
+func main() {
+	fmt.Println(1 == 1) //true
+	fmt.Println(1 == 2) //false
+
+	fmt.Println(4 <= 6)   //true
+	fmt.Println(74 >= 43) //true
+
+	fmt.Println(1 < 4) // true
+	fmt.Println(4 > 5) //false
+
+	fmt.Println(true == false) //false
+	fmt.Println(true != false) //true
+
+}
