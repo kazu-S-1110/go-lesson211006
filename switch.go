ddc98eb8923ff325a8a03110b18a6651a8546426
kazu-S-1110 <67362767+kazu-S-1110@users.noreply.github.com>
2021-10-15 07:52:03 +0900

    switch文について学習。

---
+++ switch.go
@@ -0,0 +1,29 @@
+package main
+
+// switch
+
+func main() {
+	// n := 1
+	// switch n {
+	// case 1, 2:
+	// 	fmt.Println("one or two")
+	// case 3, 4:
+	// 	fmt.Println("three or four")
+	// default:
+	// 	fmt.Println("others")
+	// }
+
+	// 変数の局所性を高めたパターン
+	// switch n := 2; n {
+	// case 1, 2:
+	// 	fmt.Println("1 or 2")
+	// case 3, 4:
+	// 	fmt.Println("3 or 4")
+	// default:
+	// 	fmt.Println("others")
+	// }
+
+	// fmt.Println(n) //nは生きていない
+	// またn >2 と == 3の混在パターンはエラーを吐くので注意。
+
+}
