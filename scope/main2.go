89218a960f03b3a980dd70b9b0f1600041fc6c31
kazu-S-1110 <67362767+kazu-S-1110@users.noreply.github.com>
2021-10-24 22:32:45 +0900

    スコープについて学習。これはもう余裕す。

--- scope/main2.go
+++ scope/main2.go
@@ -1,12 +1,38 @@
 package main
 
 import (
-	f "fmt" //パッケージ名の短縮（あまり推奨されない）
+	// f "fmt" //パッケージ名の短縮（あまり推奨されない）
+	"fmt"
 	foo "foo/mypkg"
 )
 
+func appName() string {
+	const AppName = "GoApp"
+	var Version = "1.0"
+	return AppName + " " + Version
+}
+
+func Do(s string) (b string) {
+	// var b string = s //返り値にbを指定しているので重複しているためエラーを吐く
+	b = s //これが正解
+
+	// 関数内でもスコープは定義可能
+	{
+		b := "BBBB"
+		fmt.Println(b) //BBBB
+	}
+	fmt.Println(b) // AAAA
+	return b
+}
+
 func main() {
-	f.Println(foo.Max)
+	fmt.Println(foo.Max)
 	// fmt.Println(foo.min) //min not exported by package foo
-	f.Println(foo.ReturnMin()) //1
+	fmt.Println(foo.ReturnMin()) //1
+
+	fmt.Println(appName()) //GoApp 1.0
+	// fmt.Println(AppName, Version)   //undefined: AppName
+	// 呼び出そうとしている定数や変数が小文字の変数内にあるためスコープが狭くなっている。
+
+	fmt.Println(Do("AAAA")) //AAAA
 }
