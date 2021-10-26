b4538f02b978fae6d0a57f9d41352a100725cda6
kazu-S-1110 <67362767+kazu-S-1110@users.noreply.github.com>
2021-10-26 22:47:28 +0900

    flagパッケージについて学習。後々使いそう。

---
+++ packageDefault/flag/main.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+)
+
+// flag
+//コマンドラインのオプション処理
+
+func main() {
+	// optionの値を格納する変数を定義
+	var (
+		max int
+		msg string
+		x   bool
+	)
+
+	// IntVar 整数のオプション（第二引数はキーワード、第三引数はデフォルト値、第四引数はエラーが発生した際のヘルプに記載されるもの）
+	flag.IntVar(&max, "n", 32, "処理数の最大値")
+	// StringVar 文字列のオプション
+	flag.StringVar(&msg, "m", "", "処理メッセージ")
+	// BoolVar bool型のオプション
+	flag.BoolVar(&x, "x", false, "拡張オプション")
+
+	// コマンドラインをパース
+	flag.Parse()
+
+	fmt.Println("処理数の最大値 =", max)
+	fmt.Println("処理メッセージ =", msg)
+	fmt.Println("拡張オプション =", x)
+
+	// go run main.go -n 39 -m yeeee -xって実行すると。。。
+}
