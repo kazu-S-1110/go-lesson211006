30102920bc6d930fbb48f69bbbd9fc9ccd7866cc
kazu-S-1110 <67362767+kazu-S-1110@users.noreply.github.com>
2021-10-31 20:05:55 +0900

    sliceとsliceStableを学習。理解できてるけど説明がちょっと、、、

--- packageDefault/sort/main.go
+++ packageDefault/sort/main.go
@@ -23,4 +23,31 @@ func main() {
 
 	fmt.Println("after sort", i, s) //[3 3 4 4 4 5 5 5 6 23 56 77 234 234] [a f g s]
 
+	// slice,sliceStable
+	el := []Entry{
+		{"A", 20},
+		{"F", 30},
+		{"e", 50},
+		{"g", 10},
+		{"c", 550},
+		{"e", 640},
+		{"h", 1230},
+		{"Z", 20},
+		{"E", 60},
+	}
+	fmt.Println(el)
+
+	// slice
+	sort.Slice(el, func(i, j int) bool { return el[i].Name < el[j].Name }) //名前の昇順にソートする
+	fmt.Println("after sort by Name", el)
+	sort.Slice(el, func(i, j int) bool { return el[i].Value < el[j].Value })
+	fmt.Println("after sort by Value", el)
+
+	// sliceStable
+	sort.SliceStable(el, func(i, j int) bool { return el[i].Name < el[j].Name })
+	fmt.Println("after sort with sliceStable", el)
+	sort.SliceStable(el, func(i, j int) bool { return el[i].Value < el[j].Value })
+	fmt.Println("after sort with sliceStable", el)
+
+	//slice,sliceStableの違いは同じkeyやvalueがあった場合のsortの挙動をどうするのか
 }
