e7cc6664d5295b87cc6123fa1b6ec9f4cd9e8320
kazu-S-1110 <67362767+kazu-S-1110@users.noreply.github.com>
2021-10-31 11:50:16 +0900

    waitGroupについて学習。非同期処理の制御って大変だな〜。

--- packageDefault/sync/main.go
+++ packageDefault/sync/main.go
@@ -3,13 +3,12 @@ package main
 import (
 	"fmt"
 	"sync"
-	"time"
 )
 
 // sync
 // ミューテックスによる同期処理
 
-var st struct{ A, B, C int }
+// var st struct{ A, B, C int }
 
 // これがだめな例 (同時に一つの変数にアクセスしようとするので衝突が起きてしまう)
 // func UpdataAndPrint(n int) {
@@ -36,37 +35,67 @@ var st struct{ A, B, C int }
 // 	}
 // }
 
-// ミューテックスを定義
-var mutex *sync.Mutex
+// // ミューテックスを定義
+// var mutex *sync.Mutex
 
-func UpdateAndPrint(n int) {
-	// ロックして他の関数がアクセスできないようにする(排他制御)
-	mutex.Lock()
+// func UpdateAndPrint(n int) {
+// 	// ロックして他の関数がアクセスできないようにする(排他制御)
+// 	mutex.Lock()
 
-	st.A = n
-	time.Sleep(time.Microsecond)
-	st.B = n
-	time.Sleep(time.Microsecond)
-	st.C = n
-	time.Sleep(time.Microsecond)
-	fmt.Println(st)
+// 	st.A = n
+// 	time.Sleep(time.Microsecond)
+// 	st.B = n
+// 	time.Sleep(time.Microsecond)
+// 	st.C = n
+// 	time.Sleep(time.Microsecond)
+// 	fmt.Println(st)
 
-	mutex.Unlock()
-}
+// 	mutex.Unlock()
+// }
+
+// func main() {
+// 	// mutexを初期化
+// 	mutex = new(sync.Mutex)
+
+// 	for i := 0; i < 5; i++ {
+// 		go func() {
+// 			for i := 0; i < 1000; i++ {
+// 				UpdateAndPrint(i)
+// 			}
+// 		}()
+// 	}
+
+// 	for {
+// 	}
+
+// }
+
+// 全てのゴルーチンの終了を待ち受ける
 
 func main() {
-	// mutexを初期化
-	mutex = new(sync.Mutex)
+	// sync.WaitGroupを生成
+	wg := new(sync.WaitGroup)
+	// 待ち受ける個数を指定
+	wg.Add(3)
+	go func() {
+		for i := 0; i < 100; i++ {
+			fmt.Println("1st goroutine", i)
+		}
+		wg.Done()
+	}()
+	go func() {
+		for i := 0; i < 100; i++ {
+			fmt.Println("2nd goroutine", i)
+		}
+		wg.Done()
+	}()
+	go func() {
+		for i := 0; i < 100; i++ {
+			fmt.Println("3rd goroutine", i)
+		}
+		wg.Done()
+	}()
 
-	for i := 0; i < 5; i++ {
-		go func() {
-			for i := 0; i < 1000; i++ {
-				UpdateAndPrint(i)
-			}
-		}()
-	}
-
-	for {
-	}
-
+	wg.Wait()
+	fmt.Println("all done!")
 }
