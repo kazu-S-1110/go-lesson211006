59c646c4f12c3f1ab733ef8713ecd75c51d5c63b
kazu-S-1110 <67362767+kazu-S-1110@users.noreply.github.com>
2021-11-02 07:53:41 +0900

    データの一部取得を学習。

--- thirdPackage/sqlite/main.go
+++ thirdPackage/sqlite/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"database/sql"
+	"fmt"
 	"log"
 
 	_ "github.com/mattn/go-sqlite3"
@@ -10,6 +11,11 @@ import (
 // database + sqlite3
 
 var Db *sql.DB
+
+type Person struct {
+	Name string
+	Age  int
+}
 
 func main() {
 	Db, _ := sql.Open("sqlite3", "./example.sql") //ファイルがなければ作ってくれる
@@ -23,13 +29,28 @@ func main() {
 
 	// add data
 	// cmd := "INSERT INTO persons (name, age) VALUES (?, ?)" //?とすると後で入れられるようになる
-	// _, err := Db.Exec(cmd, "hoge", 20)
+	// _, err := Db.Exec(cmd, "fooo", 40)
 	// またSQLインジェクション対策にもなるらしい
 
 	// update data
-	cmd := "UPDATE persons SET age = ? WHERE name = ?"
-	_, err := Db.Exec(cmd, 32, "hoge")
+	// cmd := "UPDATE persons SET age = ? WHERE name = ?"
+	// _, err := Db.Exec(cmd, 32, "hoge")
+	// if err != nil {
+	// 	log.Fatalln(err)
+	// }
+
+	// get data specific
+	cmd := "SELECT * FROM persons where age = ?"
+	//QueryRow get 1record
+	row := Db.QueryRow(cmd, 32)
+	var p Person
+	err := row.Scan(&p.Name, &p.Age)
 	if err != nil {
-		log.Fatalln(err)
+		if err == sql.ErrNoRows {
+			log.Println("No row")
+		} else {
+			log.Println(err)
+		}
 	}
+	fmt.Println(p.Name, p.Age)
 }
