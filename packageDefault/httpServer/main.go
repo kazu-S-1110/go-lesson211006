167314a72a250a35e279eaaee0caa5e62feb7e2b
kazu-S-1110 <67362767+kazu-S-1110@users.noreply.github.com>
2021-11-01 22:08:49 +0900

    URL毎に処理を変えられるよう学習。これでできるなんて不思議やな〜。

--- packageDefault/httpServer/main.go
+++ packageDefault/httpServer/main.go
@@ -1,21 +1,31 @@
 package main
 
 import (
-	"fmt"
+	"html/template"
+	"log"
 	"net/http"
 )
 
 // net/http
 // server side
 
-type MyHandler struct{}
+// type MyHandler struct{}
 
-// これの問題点はURLのどこにアクセスしても同じ画面になる、なのでParseFileを使う
-func (h *MyHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Hello World!")
+// // これの問題点はURLのどこにアクセスしても同じ画面になる、なのでParseFileを使う
+// func (h *MyHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+// 	fmt.Fprintf(w, "Hello World!")
+// }
+
+func top(w http.ResponseWriter, r *http.Request) {
+	t, err := template.ParseFiles("tmpl.html")
+	if err != nil {
+		log.Println(err)
+	}
+	t.Execute(w, "Hello World111!") //tmpl.htmlの{{}}の.を書き換えているらしい
+
 }
 
 func main() {
-	// http.HandleFunc("/top",top)
-	http.ListenAndServe(":8080", &MyHandler{})
+	http.HandleFunc("/top", top)
+	http.ListenAndServe(":8080", nil)
 }
