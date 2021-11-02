5eb04a9edcb85f1a6d33077152acbedd299b778d
kazu-S-1110 <67362767+kazu-S-1110@users.noreply.github.com>
2021-11-02 12:53:21 +0900

    postgreのCを学習。違いはちょとだけ

--- thirdPackage/posegre/main.go
+++ thirdPackage/posegre/main.go
@@ -17,9 +17,16 @@ type Person struct {
 }
 
 func main() {
-	Db, err := sql.Open("postgres", "user=test_user dbname=test_db password=password sslmode=disable")
+	Db, err = sql.Open("postgres", "user=test_user dbname=test_db password=password sslmode=disable")
 	if err != nil {
 		log.Panicln(err)
 	}
 	defer Db.Close()
+
+	// Create
+	cmd := "INSERT INTO persons (name, age) VALUES ($1, $2)" //sqliteの違うのはここだけ
+	_, err := Db.Exec(cmd, "fooo", 20)
+	if err != nil {
+		log.Fatalln(err)
+	}
 }
