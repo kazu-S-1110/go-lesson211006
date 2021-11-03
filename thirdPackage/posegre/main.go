7f4d995ab5b68e5de5a42c2ea93e20fe362b5e77
kazu-S-1110 <67362767+kazu-S-1110@users.noreply.github.com>
2021-11-03 19:34:25 +0900

    postgreのCRUDを学習。

--- thirdPackage/posegre/main.go
+++ thirdPackage/posegre/main.go
@@ -24,9 +24,59 @@ func main() {
 	defer Db.Close()
 
 	// Create
-	cmd := "INSERT INTO persons (name, age) VALUES ($1, $2)" //sqliteの違うのはここだけ
-	_, err := Db.Exec(cmd, "fooo", 20)
+	// cmd := "INSERT INTO persons (name, age) VALUES ($1, $2)" //sqliteの違うのはここだけ
+	// _, err := Db.Exec(cmd, "fooo", 20)
+	// if err != nil {
+	// 	log.Fatalln(err)
+	// }
+
+	// Read
+	// cmd := "SELECT * FROM persons where age = $1"
+	// //QueryRow get 1record
+	// row := Db.QueryRow(cmd, 20)
+	// var p Person
+	// err := row.Scan(&p.Name, &p.Age)
+	// if err != nil {
+	// 	if err == sql.ErrNoRows {
+	// 		log.Println("No row")
+	// 	} else {
+	// 		log.Println(err)
+	// 	}
+	// }
+	// fmt.Println(p.Name, p.Age)
+
+	// // get dates
+	// cmd = "SELECT * FROM persons"
+	// // Query は条件に合うものを全て取得
+	// rows, _ := Db.Query(cmd)
+	// defer rows.Close()
+	// var pp []Person
+	// for rows.Next() { //ちょといつもと違う書き方なので注意
+	// 	var p Person
+	// 	err := rows.Scan(&p.Name, &p.Age)
+	// 	if err != nil {
+	// 		log.Println(err)
+	// 	}
+	// 	fmt.Println(p)
+	// 	pp = append(pp, p)
+	// }
+
+	// for _, p := range pp {
+	// 	fmt.Println(p.Name, p.Age)
+	// }
+
+	// Update
+	// cmd := "UPDATE persons SET age = $1 WHERE name = $2"
+	// _, err := Db.Exec(cmd, 3434, "fooo")
+	// if err != nil {
+	// 	log.Fatalln(err)
+	// }
+
+	// Delete
+	cmd := "DELETE FROM persons WHERE name = $1"
+	_, err := Db.Exec(cmd, "fooo")
 	if err != nil {
 		log.Fatalln(err)
 	}
+
 }
